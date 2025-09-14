f00982148f72973179f5bdee4cc028c815ab997c
vishalgoswami <115400357+VishalGoswamii@users.noreply.github.com>
2025-09-14 16:03:24 +0530

    Create contracts.ts

---
+++ web/lib/contracts.ts
@@ -0,0 +1,5 @@
+export const ADDRS = {
+CARD: process.env.NEXT_PUBLIC_CARD_COLLECTION as `0x${string}`,
+VEND: process.env.NEXT_PUBLIC_VENDING_MACHINE as `0x${string}`,
+ESCROW: process.env.NEXT_PUBLIC_TRADING_ESCROW as `0x${string}`,
+};
