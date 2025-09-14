143e7ce084e9bdb5e83b4837e1d11e7ed7608f61
vishalgoswami <115400357+VishalGoswamii@users.noreply.github.com>
2025-09-15 00:34:14 +0530

    Update next.config.mjs

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-async headers() {
-return [
-{
-source: "/(.*)",
-headers: [
-{ key: "X-Frame-Options", value: "ALLOWALL" },
-{ key: "Content-Security-Policy", value: "frame-ancestors *" }
-]
-}
-];
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          { key: "Content-Security-Policy", value: "frame-ancestors *" }
+        ]
+      }
+    ];
+  }
 };
 export default nextConfig;
